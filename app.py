5eca4d82301044c9953c47759e8cc33810567d83
Prateek Madhikar <prateekmadhikar@yahoo.com>
2017-07-07 16:57:52 -0400

    Add basic app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+app.debug = True
+sockets = Sockets(app)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello World', 200
+
+# Uncomment the following to run locally and run 'python chat.py' from the root folder
+# if __name__ == "__main__":
+#     from gevent import pywsgi
+#     from geventwebsocket.handler import WebSocketHandler
+#     server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+#     server.serve_forever()
